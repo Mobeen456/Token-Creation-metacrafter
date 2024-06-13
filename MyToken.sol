664772319f21b733700fa78008aa4e5edb1e4fef
Mobeen456 <126558598+Mobeen456@users.noreply.github.com>
2024-06-13 14:51:47 +0530

    First Commit to MyToken Creation

---
+++ MyToken.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.18;
+
+contract MyToken {
+    string public Token_Name = "Mobeen";
+    string public Token_Abbrv = "hlhiknk";
+    uint public Token_TotalSupply = 0;
+
+    mapping(address => uint) public Balance;
+
+    function mint(address Addr, uint Val) public {
+        Token_TotalSupply += Val;
+        Balance[Addr] += Val;
+    }
+
+    function burn(address Addr, uint Val) public {
+        if (Balance[Addr] >= Val) {
+            Token_TotalSupply -= Val;
+            Balance[Addr] -= Val;
+        }
+    }
+}
